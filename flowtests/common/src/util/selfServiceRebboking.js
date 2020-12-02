371cc6d7137d35aa36cb1d147a9903e89d1bcd79
Hillevi Runesdotter <hillevi.runesdotter@etraveligroup.com>
2020-12-02 08:40:00 +0100

    WEB-4569 Update selfServiceRebookingSemiAutomated.js.js semi automated test

---
+++ flowtests/common/src/util/selfServiceRebboking.js
@@ -0,0 +1,80 @@
+/* eslint-disable no-console */
+import { t } from 'testcafe';
+import { createVoucherInEdvin, getDiscountCodeUrl } from '../rf_pages/edvin';
+import config from '../../../frontE2E/testdata.json';
+import { makeSearch, selectTravelers } from '../rf_pages/start';
+import resultModule from '../rf_modules/resultModule';
+import { selectTripNumber } from '../rf_pages/result';
+import { addTravelerInformation, bookFlight } from '../rf_pages/travelerDetails';
+import { addNoExtraProducts } from '../rf_pages/travelerDetailsProducts';
+import { closeSeatMapModal } from '../rf_pages/seatMap';
+import { acceptPriceChange, payWithCreditCard } from '../rf_pages/payment';
+import { waitForOrderPageToLoad } from '../rf_pages/order';
+import orderModule from '../rf_modules/orderModule';
+import edvinModule from '../rf_modules/edvinModule';
+import { scrollToElement } from './clientFunction';
+import {
+  addNumberToTraveler,
+  getFirstAdult,
+  getFirstChild,
+  getFirstInfant,
+  getSecondAdult,
+} from './travelerData';
+import { getSiteUrl } from './common';
+import setProps from './props';
+import { selectProvider } from './debugOptions';
+import enableDebug from './debug';
+
+export async function prepareSelfServiceRebookingFlow(url, props) {
+  await t.navigateTo(url);
+  await enableDebug();
+  await selectProvider('Sabre');
+  await setProps(props);
+  await t.navigateTo(getDiscountCodeUrl());
+  console.log('Go to SSR start page');
+}
+
+export async function createOrderAndDiscountCode() {
+  const urlEdvin = getSiteUrl('gotogate-uk-edvin', config.host);
+  const travelers = addNumberToTraveler([
+    getFirstAdult(),
+    getSecondAdult(),
+    getFirstChild(),
+    getFirstInfant(),
+  ]);
+  const numberOfAdults = 2;
+  const numberOfChildren = 1;
+  const numberOfInfants = 1;
+  const origin = 'Mauritius';
+  const destination = 'New Delhi';
+  const site = 'https://gotogate-uk';
+
+  await selectTravelers(numberOfAdults, numberOfChildren, numberOfInfants);
+  console.log('Search for trip');
+  await makeSearch('one way trip', origin, destination, 10);
+  await t.expect(resultModule.toggleFilterButton.visible).ok('', { timeout: 20000 });
+  await scrollToElement('[data-testid="resultPage-toggleFiltersButton-button"]');
+  await t
+    .click(resultModule.toggleFilterButton)
+    .click(resultModule.clearAirlines)
+    .click(resultModule.filterAirlineMauritiusCheckbox)
+    .click(resultModule.toggleFilterButton);
+  await selectTripNumber(0);
+  await addTravelerInformation(travelers);
+  await addNoExtraProducts(numberOfAdults + numberOfChildren);
+  await bookFlight();
+  await closeSeatMapModal();
+  await payWithCreditCard();
+  await acceptPriceChange();
+  await waitForOrderPageToLoad();
+  const orderNumber = await orderModule.orderNumber.innerText;
+  console.log('Order number: ', orderNumber);
+  await t.navigateTo(`${urlEdvin}/order/Order.list.action?_s=true&searching=true`);
+  if (await edvinModule.userNameInput.visible) {
+    await t
+      .typeText(edvinModule.userNameInput, 'autotest')
+      .typeText(edvinModule.passwordInput, 'gurkburk')
+      .click(edvinModule.logInButton);
+  }
+  await createVoucherInEdvin(site, orderNumber);
+}
---
+++ flowtests/semi-automated/src/tests/selfServiceRebookingSemiAutomated.js
@@ -0,0 +1,54 @@
+/* eslint-disable no-console */
+import { t } from 'testcafe';
+import enableDebug from '../../../common/src/util/debug';
+import { acceptCookies, getSiteUrl } from '../../../common/src/util/common';
+import { selectProvider } from '../../../common/src/util/debugOptions';
+import setProps from '../../../common/src/util/props';
+import { closeHeaderUrgencyBanner } from '../../../common/src/rf_pages/start';
+import config from '../../../frontE2E/testdata.json';
+import { getDiscountCode, getDiscountCodeUrl } from '../../../common/src/rf_pages/edvin';
+import { getWindowWidth } from '../../../common/src/util/device';
+import {
+  prepareSelfServiceRebookingFlow,
+  createOrderAndDiscountCode,
+} from '../../../common/src/util/selfServiceRebboking';
+
+const url = getSiteUrl('gotogate-uk', config.host);
+// const urlEdvin = getSiteUrl('gotogate-uk-edvin', config.host);
+const props = {
+  'PaymentService.CascadingPaymentsBehavior.Enabled': false,
+  'Payment.FraudAssessment.Accertify.ShadowMode': true,
+  'Payment.provider.creditcard': 'adyen',
+  'Result.SelfServiceRebooking.ValidWithVoucherTag.Enable': true,
+  'Result.SelfServiceRebooking.ValidWithVoucherSwitch.Enable': true,
+};
+
+fixture('Verify self service rebooking flow semi automated')
+  .page(url)
+  .beforeEach(async () => {
+    await enableDebug();
+    await selectProvider('Sabre');
+    await setProps(props);
+    await acceptCookies();
+    await closeHeaderUrgencyBanner();
+  });
+/*
+  To make this test run without errors after first deploy, an order search needs to be done manually in Edvin
+  or run this test:
+  testcafe "chrome" flowtests/frontE2E/src/tests/*Test.js -e -q -t 'Edvin warm up' --disable-multiple-windows
+
+  To start this test in terminal:
+ testcafe "chrome" flowtests/semi-automated/src/tests/*.js -e -t 'Create order in self service rebooking flow semi automated' --disable-multiple-windows
+ */
+test('Create order in self service rebooking flow semi automated', async () => {
+  if ((await getWindowWidth()) < 970) {
+    // eslint-disable-next-line no-console
+    console.warn('This test is not run on mobile or tablet device');
+  } else {
+    await createOrderAndDiscountCode();
+    console.log('Voucher code: ', getDiscountCode());
+    console.log('Voucher url: ', getDiscountCodeUrl());
+    await prepareSelfServiceRebookingFlow(url, props);
+    await t.debug();
+  }
+});
